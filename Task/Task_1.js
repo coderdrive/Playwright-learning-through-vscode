49d421fefe78dd3c227a86e9d4c65f6639c07879
Testing User <test@example.com>
2026-02-27 15:38:22 +0530

    Add Task folder and update code

---
+++ Task/Task_1.js
@@ -0,0 +1,22 @@
+// Write a program that calculates and displays the letter grade for a given numerical score (e.g., A, B, C, D, or F) based on the following grading scale:
+
+
+let score =-12;
+if (score >=90 && score <=100){
+console.log("A");
+}
+else if (score >=80 && score <=89){
+ console.log("B");
+}
+else if (score >=70 && score <=79){
+ console.log("C");
+}
+else if (score >=60 && score <=69){
+ console.log("D");
+}
+else if (score >=0 && score <=59){
+ console.log("F");
+}
+else {
+     console.log("Please type correct score"
+)}
